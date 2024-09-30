371ec1fb979798451cd8ca94a9f07ab244abd478
thelooter <evekolb2204@gmail.com>
2024-10-01 01:51:17 +0200

    Try adding posthog inline

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import posthog from 'posthog-js/dist/module.full.no-external'
+posthog.init(process.env.POSTHOG_API_KEY ?? "", { api_host: process.env.POSTHOG_API_HOST, person_profiles: 'always' })
+</script>
 
 <template>
     <NavBar></NavBar>
