e3d0a55e28e7bdde2344add127579be34400b4b5
thelooter <evekolb2204@gmail.com>
2024-10-17 18:22:57 +0200

    Remove posthog for now until I can get cookie banner working

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import posthog from 'posthog-js/dist/module.full.no-external'
-posthog.init(process.env.POSTHOG_API_KEY ?? "", { api_host: process.env.POSTHOG_API_HOST, person_profiles: 'always' })
+// import posthog from 'posthog-js/dist/module.full.no-external'
+// posthog.init(process.env.POSTHOG_API_KEY ?? "", { api_host: process.env.POSTHOG_API_HOST, person_profiles: 'always' })
 </script>
 
 <template>
