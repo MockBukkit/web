4f9399f5d21418d0e26e9bd9705e7c63547a7433
Thorinwasher <ragnarlol96@gmail.com>
2024-10-21 10:31:15 +0200

    fix(footer): Avoid touching corners / readability

--- components/footer/Footer.vue
+++ components/footer/Footer.vue
@@ -66,10 +66,10 @@ const footer_categories = [
             <img
                 src="../../assets/logo.png"
                 alt="Full Logo"
-                class="h-auto w-1/6 max-md:mx-auto max-md:my-4 max-md:w-2/3"
+                class="h-auto w-1/6 max-md:mx-auto max-md:my-4 max-md:w-2/3 max-xl:ml-4"
             />
             <div>
-                <p class="text-sm text-neutral-400 max-md:text-center">
+                <p class="text-sm text-neutral-400 max-md:text-center max-xl:mr-4">
                     © 2024 The Mockbukkit Team.
                 </p>
             </div>
