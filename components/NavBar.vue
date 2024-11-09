beea720e238ba18a6cbf7d2e24b402dbe3a72c21
thelooter <evekolb2204@gmail.com>
2024-11-09 20:18:17 +0100

    chore(docs): update docs link i missed before

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -39,7 +39,7 @@
                         </NuxtLink>
                     </li>
                     <li>
-                        <a target="_blank" rel="noopener noreferrer" href="https://mockbukkit.readthedocs.io/en/v1.21"
+                        <a target="_blank" rel="noopener noreferrer" href="https://docs.mockbukkit.org"
                             title="Docs"
                             class="block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-green-500 dark:text-white dark:hover:bg-gray-700 dark:hover:dark:text-white md:dark:hover:bg-transparent md:dark:hover:text-green-500">Docs</a>
                     </li>
