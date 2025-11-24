ea475f4fafa392c3c28c6513bdd089bb3e844cbd
thelooter <evekolb2204@gmail.com>
2025-11-24 19:59:00 +0100

    feat: update to Nuxt 4
    Signed-off-by: thelooter <evekolb2204@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import withNuxt from "./.nuxt/eslint.config.mjs";
+import withNuxt from ".nuxt/eslint.config.mjs";
 
 export default withNuxt([
     {
